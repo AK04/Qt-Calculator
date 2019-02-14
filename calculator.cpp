4c3c3431f6e276a3eb9fa66ead72fe856ca497cc
Albin Kottaram <albindoesgames@gmail.com>
2019-02-14 20:56:45 +0530

    Added slots for each numPressed

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,26 @@
 #include "calculator.h"
 #include "ui_calculator.h"
 
+double calcVal = 0.0;
+bool divTrigger = false;
+bool mulTrigger = false;
+bool addTrigger = false;
+bool subTrigger = false;
+
 Calculator::Calculator(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Calculator)
 {
     ui->setupUi(this);
+
+    ui->Display->setText(QString::number(calcVal));
+    QPushButton *numButtons[10];
+
+    for(int i = 0; i < 10; ++i) {
+        QString butName = "Button" + QString::number(i);
+        numButtons[i] = Calculator::findChild<QPushButton *>(butName);
+        connect(numButtons[i], SIGNAL(released()), this, SLOT(NumPressed()));
+    }
 }
 
 Calculator::~Calculator()
